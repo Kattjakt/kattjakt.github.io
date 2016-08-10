906943671c9f3756250b2a58f38574fdc5b44977
Leo Sandström <leo.sandstrom@dohi.se>
2016-08-10 12:53:36 +0200

    Add ability to switch between fullscreen images

--- src/app/+gallery/gallery.component.ts
+++ src/app/+gallery/gallery.component.ts
@@ -14,8 +14,31 @@ import { PhotoComponent } from '../+photo/photo.component';
 })
 export class GalleryComponent {
   photos: Photo[];
+  current: Photo;
+  index: number;
 
   constructor (manager: Manager) {
     this.photos = manager.getPhotos();
+    this.index = 0;
+
+    this.current = this.photos[this.index];
+  }
+
+  next () {
+    if (this.index === this.photos.length - 1) {
+      return;
+    }
+
+    this.index++;
+    console.info('next')
+  }
+
+  previous () {
+    if (this.index === 0) {
+      return;
+    }
+
+    this.index--;
+    console.info('previous')
   }
 }
